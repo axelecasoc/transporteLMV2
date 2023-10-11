5618208aee2cf2ed183300c7af76bd1fb7ef5e6b
Juan Alcaraz <axelec@hotmail.com>
2023-10-11 08:03:36 -0300

    intro a Java

---
+++ Clase 13 - js/ejemplo01.js
@@ -0,0 +1,14 @@
+const nombre = 'juan';
+const edad = 51;
+
+
+
+
+
+
+/*como lo vemos*/
+
+document.write(nombre + " asi que tienes " + edad);
+
+console.log(nombre)
+console.log(`Mi nombre es ${nombre} y tengo ${edad} años.`)
---
+++ Clase 13 - js/ejemplo03.js
@@ -0,0 +1,20 @@
+const frutas = ['Manzana','Banana','Pera', 'Palta'];
+
+console.log(frutas)
+//document.write(frutas)
+console.log(frutas.length); // 4
+
+console.log(frutas[1]); //Banana
+
+var ultimo = frutas[frutas.length-1]; //Palta // siempre imprime el ultimo
+
+console.log(ultimo)
+
+let seleccionado = 2;
+
+console.log(frutas[seleccionado]); //pera
+
+
+seleccionado = 1
+
+console.log(frutas[seleccionado]); // Banana
